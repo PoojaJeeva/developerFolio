4d86e378418fdecb9c4cf7c63f11e4edf8a393c2
vchidu <90011719+vchidu@users.noreply.github.com>
2024-01-08 19:08:21 -0600

    Updates

---
+++ precache-manifest.9d99d13fada9d1cfc3a53515c08ab11c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0354092bab1ce9458e86ca742d16a5",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "02b0d4f5e4648a9b6113",
+    "url": "/developerFolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "7f680d60f5e8ead03f4f",
+    "url": "/developerFolio/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "10db68b63050e2eba8c4",
+    "url": "/developerFolio/static/css/main.d8b13d37.chunk.css"
+  },
+  {
+    "revision": "eba1ad1179735244c73a",
+    "url": "/developerFolio/static/js/2.61aa7796.chunk.js"
+  },
+  {
+    "revision": "d4d701abc30fcef5f714584a64df6e82",
+    "url": "/developerFolio/static/js/2.61aa7796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b0d4f5e4648a9b6113",
+    "url": "/developerFolio/static/js/3.ffe2ac71.chunk.js"
+  },
+  {
+    "revision": "7f680d60f5e8ead03f4f",
+    "url": "/developerFolio/static/js/4.38809caa.chunk.js"
+  },
+  {
+    "revision": "10db68b63050e2eba8c4",
+    "url": "/developerFolio/static/js/main.a615eb8a.chunk.js"
+  },
+  {
+    "revision": "c5d995d5461f9aa09ab1",
+    "url": "/developerFolio/static/js/runtime-main.4a0c808c.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "d1a6eea88586ed34fbf8c6e3a9f4aa00",
+    "url": "/developerFolio/static/media/Suffolk.d1a6eea8.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "4b6ac6fc8e6381f7f46aa77d8ff5df6a",
+    "url": "/developerFolio/static/media/fema.4b6ac6fc.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
